8bd2ed0080393f27c7e68e1c37de581f16bafebc
SuperXoma132 <61152530+SuperXoma132@users.noreply.github.com>
2023-02-09 14:39:22 +0300

    app1

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print()
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+import sys
+def application():
+    app = QApplication(sys.argv)
+    window = QMainWindow()
+    window.setWindowTitle("Lev Zigomet")
+    window.setGeometry(300,300,400,400)
+    window.show()
+    sys.exit(app.exec_())
+if __name__ == "__main__":
+    application()
